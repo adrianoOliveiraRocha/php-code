f7d563b410b5a5587ca6efee518f949ddee8b842
adriano <adrianoOliveiraRocha>
2020-04-15 15:47:10 -0300

    #

--- design-patterns/creational-design-patterns/abstractory-factory/example1/index.php
+++ design-patterns/creational-design-patterns/abstractory-factory/example1/index.php
@@ -10,4 +10,18 @@ require_once(__DIR__ . '/src/toys/PuzzleToy.php');
 require_once(__DIR__ . '/src/toys/SFMazeToy.php');
 require_once(__DIR__ . '/src/toys/SFPuzzleToy.php');
 require_once(__DIR__ . '/src/toys/UKMazeToy.php');
-require_once(__DIR__ . '/src/toys/UKPuzzleToy.php ');
+require_once(__DIR__ . '/src/toys/UKPuzzleToy.php');
+
+
+
+$sanFranciscoFactory = new \Factory\SFToyFactory();
+var_dump($sanFranciscoFactory->makeMaze());
+echo "\n";
+var_dump($sanFranciscoFactory->makePuzzle());
+echo "\n";
+
+$britishToyFactory = new \Factory\UKToyFactory();
+var_dump($britishToyFactory->makeMaze());
+echo "\n";
+var_dump($britishToyFactory->makePuzzle());
+echo "\n";
