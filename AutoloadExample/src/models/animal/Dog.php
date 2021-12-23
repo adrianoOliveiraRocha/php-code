ab9c6e1e516aece0423f9ccf65af61feb02d79f3
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-12-23 10:18:28 -0300

    #

--- AutoloadExample/src/models/animal/Dog.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace models\animal;
-
-class Dog {
-  public function __construct() {
-    echo "<p>Hello! I am a dog!</p>";
-  }
-}
--- AutoloadExample/src/models/car/Fusca.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace models\car;
-
-class Fusca {
-  public function __construct() {
-    echo "<p>Hello! I am a fusca!</p>";
-  }
-}
