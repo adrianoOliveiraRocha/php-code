6f6f0f532fcb111705e92379cb58c62c152c30dc
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2021-12-29 08:20:48 -0300

    #

--- Algorithms/test.php
+++ Algorithms/test.php
@@ -1,40 +1,16 @@
 <?php
 
-// represent this graph => Understanding_php_arrays/img/graph.png
-
-$graph = [];
-$nodes = ['A', 'B', 'C', 'D', 'E'];
-foreach($nodes as $x) {
-  foreach($nodes as $y) {
-    $graph[$x][$y] = 0;
-  }
+class Player {
+  public $name;
+  public $country;
+  public $age;
+  public $currentTeam;
 }
 
-$graph["A"]["B"] = 1;
-$graph["B"]["A"] = 1;
-$graph["A"]["C"] = 1;
-$graph["C"]["A"] = 1;
-$graph["A"]["E"] = 1;
-$graph["E"]["A"] = 1;
-$graph["B"]["E"] = 1;
-$graph["E"]["B"] = 1;
-$graph["B"]["D"] = 1;
-$graph["D"]["B"] = 1;
+$r = new Player();
+$r->name = "Ronaldo";
+$r->country = "Brazil";
+$r->age = 45;
+$r->currentTeam = "Barcelona";
 
-foreach ($nodes as $x) {
-  foreach ($nodes as $y) {
-    echo $x, $y . "\t";
-  }
-  echo "\n";
-}
-
-echo "\n";
-
-foreach ($nodes as $x) {
-  foreach ($nodes as $y) {
-    echo $graph[$x][$y] . "\t";
-  }
-  echo "\n";
-}
-
-// After of to define connections => Understanding_php_arrays/img/connections_defined.png
+print_r($r);
