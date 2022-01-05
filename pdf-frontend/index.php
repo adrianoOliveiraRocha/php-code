9c00dd0dfba4471df1d5e4c60883d5156c844788
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2022-01-05 14:03:42 -0300

    #

---
+++ pdf-frontend/index.php
@@ -0,0 +1,57 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <script src="https://unpkg.com/pdf-lib@1.4.0"></script>
+    <script src="https://unpkg.com/downloadjs@1.4.7"></script>
+  </head>
+
+  <body>
+    <input type="file" id="file1"> <br>
+    <input type="file" id="file2"> <br>
+    <button onclick="merge()">Merge</button> <br>
+    <a id="link">Download</a>
+  </body>
+
+  <script>
+    
+    function readFileAsync(file) {
+      return new Promise((resolve, reject) => {
+        let reader = new FileReader(); 
+        reader.onload = () => {
+            resolve(reader.result);
+        }; 
+        reader.onerror = reject; 
+        reader.readAsArrayBuffer(file);
+      })
+    }
+
+    function download(file, filename, type) {
+        const link = document.getElementById('link');
+        link.download = filename;
+        let binaryData = [];
+        binaryData.push(file);
+        link.href = URL.createObjectURL(new Blob(binaryData, {type: type}))
+    }
+
+    async function merge() {
+      let PDFDocument = PDFLib.PDFDocument;
+
+      const in1 = document.getElementById('file1').files[0];
+      const in2 = document.getElementById('file2').files[0]; 
+      let bytes1 = await readFileAsync(in1);
+      let bytes2 = await readFileAsync(in2); 
+      const pdf1 = await PDFDocument.load(bytes1);
+      const pdf2 = await PDFDocument.load(bytes2);
+
+      const mergedPdf = await PDFDocument.create(); 
+      const copiedPagesA = await mergedPdf.copyPages(pdf1, pdf1.getPageIndices());
+      copiedPagesA.forEach((page) => mergedPdf.addPage(page)); 
+      const copiedPagesB = await mergedPdf.copyPages(pdf2, pdf2.getPageIndices());
+      copiedPagesB.forEach((page) => mergedPdf.addPage(page)); 
+      const mergedPdfFile = await mergedPdf.save();
+
+      download(mergedPdfFile, 'pdf-lib_page_copying_example.pdf', 'application/pdf')
+    }
+    
+  </script>
+</html>
